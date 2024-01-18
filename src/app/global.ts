215aca668e19bc0aa29953ec4c743e07e6c0c452
Erdnando <erdnandovr@algartech.com>
2024-01-18 07:26:12 -0600

    correciones frontend

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,6 +1,6 @@
 export const Global = {
-    Url:"https://transversal-portaltls-api.shfyjbr2p4o.us-south.codeengine.appdomain.cloud/api/v1/"
+  //  Url:"https://transversal-portaltls-api.shfyjbr2p4o.us-south.codeengine.appdomain.cloud/api/v1/"
    // Url:"https://db1e-189-217-208-13.ngrok-free.app/api/v1/"
-    //Url:"http://localhost:5201/api/v1/"
+    Url:"http://localhost:5201/api/v1/"
 
 }
