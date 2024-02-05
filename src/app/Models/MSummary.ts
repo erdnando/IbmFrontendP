d8aaaf56a0728f740752a277de166712f8817622
Erdnando <erdnandovr@algartech.com>
2024-02-04 20:20:31 -0600

    spaces metricas carga file

--- src/app/Models/MSummary.ts
+++ src/app/Models/MSummary.ts
@@ -6,18 +6,18 @@ import { MRol } from "./MRol"
 export interface MetricasCarga{
 
     eN_PROCESO_ARP:string;
-    ​​eN_PROCESO_STE:string;
-    ​​eN_PROCESO_TSE:string;
-    ​​mensaje:string;
-    ​​nO_APLICA_X_HORARIO_ARP:string;
-    ​​nO_APLICA_X_HORARIO_STE:string;
-    ​​nO_APLICA_X_HORARIO_TSE: string;
-    ​​nO_APLICA_X_OVERLAPING_ARP: string;
-    ​​nO_APLICA_X_OVERLAPING_STE: string;
-    ​​nO_APLICA_X_OVERLAPING_TSE: string;
-    ​​nO_APLICA_X_OVERTIME_ARP: string;
-    ​​nO_APLICA_X_OVERTIME_STE: string;
-    ​​nO_APLICA_X_OVERTIME_TSE: string;
+    eN_PROCESO_STE:string;
+    eN_PROCESO_TSE:string;
+    mensaje:string;
+    nO_APLICA_X_HORARIO_ARP:string;
+    nO_APLICA_X_HORARIO_STE:string;
+    nO_APLICA_X_HORARIO_TSE: string;
+    nO_APLICA_X_OVERLAPING_ARP: string;
+    nO_APLICA_X_OVERLAPING_STE: string;
+    nO_APLICA_X_OVERLAPING_TSE: string;
+    nO_APLICA_X_OVERTIME_ARP: string;
+    nO_APLICA_X_OVERTIME_STE: string;
+    nO_APLICA_X_OVERTIME_TSE: string;
     arP_CARGA:string;
     stE_CARGA:string;
     tsE_CARGA:string;
