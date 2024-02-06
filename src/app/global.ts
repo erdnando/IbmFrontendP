6f738adf074bd51ec7030a937d3ca8612b3b566f
Erdnando <erdnandovr@algartech.com>
2024-02-06 08:15:13 -0600

    ajustes dashboard

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,4 +1,4 @@
 export const Global = {
-   Url:"https://transversal-portaltls-api.shfyjbr2p4o.us-south.codeengine.appdomain.cloud/api/v1/"
-   // Url:"http://localhost:5201/api/v1/"
+   //Url:"https://transversal-portaltls-api.shfyjbr2p4o.us-south.codeengine.appdomain.cloud/api/v1/"
+    Url:"http://localhost:5201/api/v1/"
 }
